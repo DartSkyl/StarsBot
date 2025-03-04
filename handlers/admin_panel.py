69c7af677895b58f4d0c1a7ed5d49155fbc12b3b
Evgeny <dartcyberart@gmail.com>
2025-03-04 19:13:56 +0300

    Beta 1.12

--- handlers/admin_panel.py
+++ handlers/admin_panel.py
@@ -398,7 +398,6 @@ async def edit_mode_for_message(callback: CallbackQuery, state: FSMContext):
     await state.set_data({'msg': msg_path})
     try:
         msg_text = (await bot_base.settings_get(msg_path))[1]
-        print(msg_text)
         msg_text = await forming_str_from_txt_file(msg_text, kwargs=msg_dict[msg_path][1])
 
         await callback.message.answer(msg_text, reply_markup=msg_setting_edit_func)
